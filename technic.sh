7697415a814afcc4411559c3442e0e6795d7eb78
Scott Gould <greysilly7@gmail.com>
2022-06-28 23:31:45 -0400

    Configure things a bit more

--- technic.sh
+++ technic.sh
@@ -1,6 +1,6 @@
 rm -rf .technic
 mkdir .technic/
 cp -R bin/ config/ local/ mods/ scripts/ logo.png .technic/
-cd .technic && java -jar ../packwiz-installer-bootstrap.jar file://../pack.toml && cd ..
+cd .technic && java -jar ../packwiz-installer-bootstrap.jar https://raw.githubusercontent.com/Project-Open-Computers/POC3/feat/switch-to-packwiz/pack.toml --bootstrap-main-jar ../packwiz-installer.jar && cd ..
 rm -rf technic/packwiz* .technic/index.toml .technic/pack.toml .technic/mods/*.toml
 zip ./Technic-ProjectOC.zip ./.technic/
